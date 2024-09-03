dd161acc39a86e23bd66957a77677390242593bf
Jay Tailor <jay8460568088@gmail.com>
2024-09-03 23:44:02 +0530

    first commit

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { MessageList } from "@/components/message-list";
+import { UseGetChannel } from "@/features/channels/api/use-get-channel";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Loader, TriangleAlert } from "lucide-react";
+import { ChatInput } from "./chat-input";
+import { Header } from "./header";
+
+const ChannelIdPage = () => {
+
+    const channelId = useChannelId();
+    const { results, status, loadMore } = useGetMessages({ channelId });
+
+    // console.log({results});
+
+    const { data: channel, isLoading: channelLoading } = UseGetChannel({ id: channelId })
+
+    if (channelLoading || status === "LoadingFirstPage") {
+        return (
+            <div className="h-full flex-1 flex items-center justify-center">
+                <Loader className="animate-spin size-5 text-muted-foreground" />
+            </div>
+        )
+    }
+
+    if (!channel) {
+        return (
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-5 text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">
+                    Channel not found
+                </span>
+            </div>
+        )
+    }
+    return (
+        <div className="flex flex-col h-full">
+            <Header title={channel.name} />
+            <MessageList
+                channelName={channel.name}
+                channelCreationTime={channel._creationTime}
+                data={results}
+                loadMore={loadMore}
+                isLoadingMore={status === "LoadingMore"}
+                canLoadMore={status === "CanLoadMore"}
+            />
+            <ChatInput placeholder={`Message # ${channel.name}`} />
+        </div>
+    )
+}
+export default ChannelIdPage
